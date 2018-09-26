0d84ebbb834f1bbc6b3e2ba51f723393afc5befd
bixBeta <bix.Beta@github.com>
2018-09-26 15:28:12 -0400

    os.rename

--- reveRse.py
+++ reveRse.py
@@ -1,10 +1,17 @@
+# python utility to create  reverse complements for NextSeq 500's i5 adapters
+# written by Faraz Ahmed 
+# FA180921
+
+# version 1.1 
+
 import os
 import sys
 from Bio.Seq import Seq
 
 
 filename = sys.argv[-1]
-sys.stdout = open('rcIndices.txt', 'w')
+sys.stdout = open('rcIndices', 'w')
+
 
 file = open(filename, "r")                  # open file handle, read only
 index = []                                  # initializes a list
@@ -17,7 +24,10 @@ file.close()                                # close file handle
 for x in range(len(index)):
     dna = Seq(index[x])
     print (dna.reverse_complement())
-    
+
+
+os.rename('rcIndices', 'ReverseComplement_' + filename)
+
 
 
 
