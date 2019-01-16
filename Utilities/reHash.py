32cfaf9d5e6216899ef9d81d2c0850802da6ec0a
BiX <33409348+bixBeta@users.noreply.github.com>
2019-01-16 12:57:15 -0500

    Update reHash.py

--- Utilities/reHash.py
+++ Utilities/reHash.py
@@ -8,11 +8,11 @@ os.getcwd( )
 # os.chdir('/Users/bix/Desktop/subset.0021/') # change dir if needed
 
 
-barCode = pd.read_csv(sys.argv[-1]) # read in the key value combinations
+barCode = pd.read_csv('key.csv') # read in the key value combinations
 
-betas = pd.read_csv(sys.argv[-2]) # read in the final dropped beta values with centrix id's as colnames
+betas = pd.read_csv('EPIC.csv') # read in the final dropped beta values with centrix id's as colnames
 
-betas.set_index(sys.argv[-3]) # specify string from the -2 argument file to use as index[i]
+betas.set_index('samples') # specify string from the -2 argument file to use as index[i]
 
 # rename based on specified key value relationship (manually change)
 finalSet = betas.rename(columns=dict(zip(barCode['centrix'], barCode['Sample_Name']))) 
