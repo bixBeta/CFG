336c4bec5dd0b8e2a56f006ae09f747393876840
BiX <33409348+bixBeta@users.noreply.github.com>
2019-02-06 11:21:00 -0500

    Update processCounts.sh

--- Differential Expression/processCounts.sh
+++ Differential Expression/processCounts.sh
@@ -13,17 +13,25 @@ elif [ "$1" = "1" ]
         do
         awk 'NR > 4 {print $1 "\t" $3}' $i > $i.rawCounts
         done
+        mkdir rawCounts
+        mv *.rawCounts rawCounts
+        
 elif [ "$1" = "2" ]
     then
         for i in *.ReadsPerGene.out.tab
         do
         awk 'NR > 4 {print $1 "\t" $4}' $i > $i.rawCounts
         done
+        mkdir rawCounts
+        mv *.rawCounts rawCounts
+        
 else
     for i in *.ReadsPerGene.out.tab
     do
     awk 'NR > 4 {print $1 "\t" $2}' $i > $i.rawCounts
     done
+    mkdir rawCounts
+    mv *.rawCounts rawCounts
 fi
 
 
